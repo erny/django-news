2c5940c396530f9a45c5f6bd5e2d1945c143fe6e
Charles Leifer <coleifer@gmail.com>
2010-08-04 23:36:53 -0500

    Clarifying how subcategorization works and adding some comments

--- news/tests/tests.py
+++ news/tests/tests.py
@@ -34,6 +34,7 @@ class NewsModelsTestCase(TestCase):
         'Programming': [
             {'title': 'Git rules', 'summary': '<p>vcs troll</p>', 'link': '/git/'},
             {'title': 'Hg rules and its python', 'summary': '<p>trolled</p>', 'link': '/hg/'},
+            {'title': 'Django stuff', 'summary': '', 'link': '/django/'},
             {'title': 'Python and Django rock', 'summary': '<p>Programming article about python and django</p>', 'link': '/p+d+rokk/'},
         ],
         'Geek': [
@@ -66,6 +67,8 @@ class NewsModelsTestCase(TestCase):
         Feed.fetch_feed = self.orig_fetch_feed
 
     def test_keyword_list(self):
+        # basically this test just makes sure that when given some comma
+        # separated keywords we can parse them into a nice list
         test_whitelist = WhiteListFilter(name='Test', keywords='test1, test2')
         self.assertEqual(test_whitelist.get_keyword_list(), ['test1', 'test2'])
         
@@ -76,11 +79,14 @@ class NewsModelsTestCase(TestCase):
         self.assertEqual(test_whitelist.get_keyword_list(), ['test1 test2'])
     
     def test_category_path_updating(self):
+        # if a top level category is saved, since the url path is cached on
+        # the model, it needs to be updated on all descendents.
         programming = Category.objects.get(name='Programming')
         programming.name = 'Progging'
         programming.slug = 'progging'
         programming.save()
         
+        # check that python's url path changed from /programming/ to /progging/
         python = Category.objects.get(name='Python')
         self.assertEqual(python.url_path, 'progging/python/')
         
@@ -92,12 +98,17 @@ class NewsModelsTestCase(TestCase):
         feed_data = feed.fetch_feed()
         apple, wallet = feed_data.entries
         
+        # apple article had some innocuous HTML in the title, but lets the
+        # settings are configured to remove it, so do it.  also remove the
+        # script tags within the summary
         apple_article = feed.sanitize_item(apple)
         self.assertEqual(apple_article.headline, 'Apple gear')
         self.assertEqual(apple_article.content, 'I <3 apple')
         self.assertEqual(apple_article.url, '/apple/')
         self.assertEqual(apple_article.guid, '/apple/')
         
+        # here just making sure that the html blacklist will kill the iframe
+        # tag while leaving the bold tags intact
         wallet_article = feed.sanitize_item(wallet)
         self.assertEqual(wallet_article.headline, 'Homemade wallets')
         self.assertEqual(wallet_article.content, 'In forty three <b>easy</b> steps ')
@@ -107,10 +118,13 @@ class NewsModelsTestCase(TestCase):
     def test_categorization_simple(self):
         geek = Category.objects.get(name='Geek')
         
+        # grab the two articles from this feed
         feed = Feed.objects.get(name='Geek')
         feed_data = feed.fetch_feed()
         apple, wallet = feed_data.entries
         
+        # sanitize the feed data to get an unsaved article instance and
+        # see that it gets categorized properly
         apple_article = feed.sanitize_item(apple)
         apple_categories = feed.get_categories_for_article(apple_article)
         
@@ -122,39 +136,64 @@ class NewsModelsTestCase(TestCase):
         self.assertEqual(wallet_categories, [geek])
     
     def test_subcategorization(self):
-        # grab the categories
+        # grab the categories from the database
         programming = Category.objects.get(name='Programming')
         python = Category.objects.get(name='Python')
         django = Category.objects.get(name='Django')
         
+        # the programming feed is interesting from a testing perspective b/c
+        # it contains a subcategory that autopopulates from it, and that
+        # subcategory has a subcategory in turn.
+        #
+        # Programming -> Python -> Django  --  a whitelist is applied to the
+        # articles, so non python articles won't make it from Programming to
+        # Python.
+        #
+        # NOTE: that an article will follow all descendent relations, so an
+        # article in Programming can skip the Python category but make it into
+        # the Django category if it passes the whitelist between Py & Django
         feed = Feed.objects.get(name='Programming')
         feed_data = feed.fetch_feed()
         
-        git, hg, py = feed_data.entries
+        git, hg, dj, py = feed_data.entries
         
+        # this article will go into the programming category directly, but
+        # it will not propagate to any descendent categories
         git_article = feed.sanitize_item(git)
         git_categories = feed.get_categories_for_article(git_article)
         
         self.assertEqual(git_categories, [programming])
         
+        # this article will also go into the python category because it
+        # has the word "python" in the headline
         hg_article = feed.sanitize_item(hg)
         hg_categories = feed.get_categories_for_article(hg_article)
         
         self.assertEqual(hg_categories, [programming, python])
         
+        # this article will go into the django category, skipping the python
+        # one altogether
+        dj_article = feed.sanitize_item(dj)
+        dj_categories = feed.get_categories_for_article(dj_article)
+        
+        self.assertEqual(dj_categories, [programming, django])
+        
+        # this article will go into all three as it has 'python' and 'django'
+        # in the headline
         py_article = feed.sanitize_item(py)
         py_categories = feed.get_categories_for_article(py_article)
         
         self.assertEqual(py_categories, [programming, python, django])
     
     def test_whitelisting(self):
-        # hacker news will go into programming if it matches python or django
         # grab the categories
         programming = Category.objects.get(name='Programming')
         python = Category.objects.get(name='Python')
         django = Category.objects.get(name='Django')
         geek = Category.objects.get(name='Geek')
         
+        # hacker news will go into programming if it matches python or django,
+        # otherwise it just goes to geek
         feed = Feed.objects.get(name='Hacker News RSS')
         feed_data = feed.fetch_feed()
         
@@ -170,6 +209,9 @@ class NewsModelsTestCase(TestCase):
         
         self.assertEqual(py_categories, [geek, programming, python])
         
+        # once again, the article passes the whitelist filter between the
+        # hacker news -> programming category, then once in the programming
+        # it will propagate down to the django category
         dj_article = feed.sanitize_item(dj)
         dj_categories = feed.get_categories_for_article(dj_article)
         
