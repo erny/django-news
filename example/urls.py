185a3bbb82a861d982a8d44a7d56a541b7845488
Charles Leifer <coleifer@gmail.com>
2010-08-04 21:20:17 -0500

    Removing brokeass example app

--- example/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from django.contrib import admin
-
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    (r'^news/', include('news.urls')),
-    (r'^admin/', include(admin.site.urls)),
-)
